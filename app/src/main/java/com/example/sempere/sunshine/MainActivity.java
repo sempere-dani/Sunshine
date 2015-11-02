3b516bde55545350d7296cd308b4436bf0eb5185
Daniel Sempere <sempere.dani@gmail.com>
2015-11-02 23:08:58 +0100

    first lesson finished

--- app/src/main/java/com/example/sempere/sunshine/MainActivity.java
+++ app/src/main/java/com/example/sempere/sunshine/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.sempere.sunshine;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       if (savedInstanceState == null) {
+                         getSupportFragmentManager().beginTransaction()
+                                 .add(R.id.fragment, new MainActivityFragment())
+                                 .commit();
+                     }
+
     }
 
 
--- app/src/main/java/com/example/sempere/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/sempere/sunshine/MainActivityFragment.java
@@ -1,22 +1,60 @@
 package com.example.sempere.sunshine;
 
-import android.support.v4.app.Fragment;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+
+
     public MainActivityFragment() {
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+
+
+        //Create an ArrayList
+        Resources res = getResources();
+        String[] forecastArray = res.getStringArray(R.array.items);
+
+        List<String> weekForecast = new ArrayList<>(Arrays.asList(forecastArray));
+
+        for (int i=0;i<=forecastArray.length;i++){
+
+            Log.i("Item ", String.valueOf(i));
+
+        }
+
+        //Array Adapter
+
+        ArrayAdapter<String> forecastAdapter  = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast,
+                                                R.id.list_item_forecast_textview,weekForecast);
+
+        View  rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        //setup Views
+        ListView listview = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listview.setAdapter(forecastAdapter);
+
+       // return inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
     }
+
 }

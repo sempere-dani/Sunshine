d45761e93110e2a6aa039fa78556d81b4c338496
Daniel Sempere <sempere.dani@gmail.com>
2015-11-10 07:53:48 +0100

    some changes

--- app/src/main/java/com/example/sempere/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/sempere/sunshine/ForecastFragment.java
@@ -1,10 +1,12 @@
 package com.example.sempere.sunshine;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,9 +14,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.text.format.Time;
+import android.widget.Toast;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,7 +29,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -97,6 +100,23 @@ public class ForecastFragment extends Fragment {
         listview.setAdapter(mForecastAdapter);
 
         // return inflater.inflate(R.layout.fragment_main, container, false);
+
+        //Creating Toast
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = mForecastAdapter.getItem(position);
+                Toast.makeText(getActivity(), forecast,
+                        Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
+            }
+
+
+
+        });
 
 
 
